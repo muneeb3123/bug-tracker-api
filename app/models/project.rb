041cd6c4c01c83a522997143dd4b0e0bd82ae556
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-05-03 23:42:50 +0500

    create project table and write validations

---
+++ app/models/project.rb
@@ -0,0 +1,7 @@
+class Project < ApplicationRecord
+    has_many :bugs, dependent: :destroy
+    has_many :user_projects
+    has_many :users, through: :user_projects
+
+    validates :name, presence: true
+end
