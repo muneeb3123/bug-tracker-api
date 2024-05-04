5a8636c565c31ed3248fa63fee7f5faea7293cd6
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-05-04 15:49:50 +0500

    validate bug can only create by Qa

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -4,10 +4,19 @@ class Bug < ApplicationRecord
   validates :deadline, presence: true
   validates :bug_type, presence: true, inclusion: { in: %w[feature bug] }
   validates :status, presence: true, inclusion: { in: %w[open started resolved completed] }
+  validate :user_is_qa, on: :create
 
   belongs_to :project
   belongs_to :user
 
   enum bug_type: { feature: 0, bug: 1 }
   enum status: { open: 0, started: 1, resolved: 2, completed: 3 }
+
+  private
+
+  def user_is_qa
+    return if user.qa?
+
+    errors.add(:user, 'must be a QA to create a bug')
+  end
 end
