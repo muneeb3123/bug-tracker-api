5d141920e2078834d8d56fa7bd47248857b4bc45
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-05-04 15:48:01 +0500

    handle unauthorized excess of cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+    rescue_from CanCan::AccessDenied do |exception|
+        render json: { error: "Access denied: #{exception.message}" }, status: :forbidden
+      end
 end
