21f3b862cde321de770751765943eb3fefc25c1f
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-05-13 05:05:57 +0500

    mail on remove user from project

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -6,4 +6,11 @@ class ProjectMailer < ApplicationMailer
 
     mail(to: email_address_with_name(@user.email, @user.name), subject: "You have been assigned a new project")
   end
+
+  def notify_user_removal
+    @user = params[:user]
+    @project = params[:project]
+
+    mail(to: email_address_with_name(@user.email, @user.name), subject: "You have been removed from a project")
+  end
 end
