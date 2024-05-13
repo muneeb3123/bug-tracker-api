ad2ee2c885820b80e5d158892d61360bfb922f1b
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-05-13 22:41:44 +0500

    modify cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "http://localhost:5173"
+      origins "https://bug-zilla.netlify.app"
   
       resource "*",
         headers: :any,
