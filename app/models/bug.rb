424db4c48dc28beebebb2fceabcee7df2fd9d2ae
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-05-13 05:25:46 +0500

    modify

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,18 +1,20 @@
 class Bug < ApplicationRecord
-
   belongs_to :project
-  belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+  belongs_to :developer, class_name: 'User', foreign_key: 'developer_id', optional: true
   has_one_attached :screenshot
 
   validates :title, presence: true, uniqueness: { scope: :project_id, message: "should be unique throughout the project" }
   validates :deadline, presence: true
   validates :bug_type, presence: true, inclusion: { in: %w[feature bug] }
   validates :status, presence: true, inclusion: { in: %w[open started resolved completed] }
-  validate :user_is_qa, on: :create
   validate :validate_attachment_filetypes, on: :create
+  some_method
 
   enum bug_type: { feature: 0, bug: 1 }
   enum status: { open: 0, started: 1, resolved: 2, completed: 3 }
+
+
 
   def screenshot_url
     Rails.application.routes.url_helpers.url_for(screenshot) if screenshot.attached?
@@ -26,12 +28,5 @@ class Bug < ApplicationRecord
     if !screenshot.content_type.in?(%w[image/png image/gif])
       errors.add(:screenshot, 'needs to be a GIF or PNG')
     end
-  end
-
-
-  def user_is_qa
-    return if user.qa?
-
-    errors.add(:user, 'must be a QA to create a bug')
   end
 end
