d0627d77cb50a4dfada0b837d25fc90ccdd220d6
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-05-14 07:07:26 +0500

    create seed file

---
+++ db/seeds.rb
@@ -0,0 +1,143 @@
+User.create(name: 'John', email: 'john@example.com', password: '123456', user_type: 'manager')
+
+# Create 10 developer users
+User.create(
+  name: "Alex Johnson",
+  email: "alex.johnson@example.com",
+  password: '123456',
+  user_type: 'developer'
+)
+
+User.create(
+  name: "Emily Chen",
+  email: "emily.chen@example.com",
+  password: '123456',
+  user_type: 'developer'
+)
+
+User.create(
+  name: "Michael Lee",
+  email: "michael.lee@example.com",
+  password: '123456',
+  user_type: 'developer'
+)
+
+User.create(
+  name: "Sophia Garcia",
+  email: "sophia.garcia@example.com",
+  password: '123456',
+  user_type: 'developer'
+)
+
+User.create(
+  name: "Ethan Kim",
+  email: "ethan.kim@example.com",
+  password: '123456',
+  user_type: 'developer'
+)
+
+User.create(
+  name: "Emma Martinez",
+  email: "emma.martinez@example.com",
+  password: '123456',
+  user_type: 'developer'
+)
+
+User.create(
+  name: "Olivia Nguyen",
+  email: "olivia.nguyen@example.com",
+  password: '123456',
+  user_type: 'developer'
+)
+
+User.create(
+  name: "Noah Patel",
+  email: "noah.patel@example.com",
+  password: '123456',
+  user_type: 'developer'
+)
+
+User.create(
+  name: "Liam Rodriguez",
+  email: "liam.rodriguez@example.com",
+  password: '123456',
+  user_type: 'developer'
+)
+
+User.create(
+  name: "Mia Wang",
+  email: "mia.wang@example.com",
+  password: '123456',
+  user_type: 'developer'
+)
+
+# Create 10 QA users
+User.create(
+  name: "Ava Ali",
+  email: "ava.ali@example.com",
+  password: '123456',
+  user_type: 'qa'
+)
+
+User.create(
+  name: "William Kim",
+  email: "william.kim@example.com",
+  password: '123456',
+  user_type: 'qa'
+)
+
+User.create(
+  name: "Isabella Patel",
+  email: "isabella.patel@example.com",
+  password: '123456',
+  user_type: 'qa'
+)
+
+User.create(
+  name: "James Nguyen",
+  email: "james.nguyen@example.com",
+  password: '123456',
+  user_type: 'qa'
+)
+
+User.create(
+  name: "Sophia Singh",
+  email: "sophia.singh@example.com",
+  password: '123456',
+  user_type: 'qa'
+)
+
+User.create(
+  name: "Lucas Kim",
+  email: "lucas.kim@example.com",
+  password: '123456',
+  user_type: 'qa'
+)
+
+User.create(
+  name: "Amelia Wong",
+  email: "amelia.wong@example.com",
+  password: '123456',
+  user_type: 'qa'
+)
+
+User.create(
+  name: "Benjamin Chen",
+  email: "benjamin.chen@example.com",
+  password: '123456',
+  user_type: 'qa'
+)
+
+User.create(
+  name: "Mia Gupta",
+  email: "mia.gupta@example.com",
+  password: '123456',
+  user_type: 'qa'
+)
+
+User.create(
+  name: "Henry Lopez",
+  email: "henry.lopez@example.com",
+  password: '123456',
+  user_type: 'qa'
+)
